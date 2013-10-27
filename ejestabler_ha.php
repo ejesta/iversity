be56924238438b2e2f865d2e2729e69ad3e5a360
ejesta <shaihaluds@gmail.com>
2013-10-27 19:17:10 +0100

    Erste Version der iversity Hausaufgabe

---
+++ ejestabler_ha.php
@@ -0,0 +1,49 @@
+<!doctype HTML>
+<html lang="de"><head>
+<meta charset="utf-8">
+
+<style type="text/css">
+	
+body {
+	margin: 0 auto;
+	padding: 0 10% 0 10%;
+	width: 80%;
+	height: 100%;
+}
+header {
+	padding: 40px 0px; 
+	background: #f5f5f5;
+}
+
+footer {
+	padding: 40px 0px; 
+	background: #f5f5f5;
+
+}
+
+#content {
+	padding: 200px 0px; 
+	background: #ff5c5c;
+		color: #fff
+}
+
+h1, h2 {
+	margin-left: 20%;
+	font-family: verdana, helvetica, arial, sans-serif; 
+}
+</style>
+
+<title>iversity HA – Eje Stabler</title>
+
+</head>
+<body>
+	<header>
+	<h2>Header Bereich</h2>
+	</header>
+	<div id="content"><h1>Content Bereich</h1></div>
+	<footer><h2>Footer</h2>
+	</footer>
+	
+
+</body>
+</html>
